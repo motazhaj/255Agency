b2c7ed7c88476e66c8244e38be8bb34e90f31d4e
Motaz HM <motazhajm@gmail.com>
2024-11-26 15:37:45 +0200

    Add tailwind configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,26 @@ module.exports = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primary: "#fc5508",
+        secondary: "#fef701",
+      },
+      fontFamily: {
+        sans: ["Helvetica", "Arial", "sans-serif"],
+        serif: ["Merriweather", "serif"],
+      },
+      spacing: {
+        128: "32rem", // Custom spacing, useful for larger layouts
+        144: "36rem",
+      },
+      screens: {
+        sm: "640px", // Mobile: from 640px and above
+        md: "768px", // Tablet: from 768px and above
+        lg: "1024px", // Desktop: from 1024px and above
+        xl: "1280px", // Large desktop: from 1280px and above
+      },
+      boxShadow: {
+        xl: "0 4px 6px rgba(0, 0, 0, 0.1), 0 1px 3px rgba(0, 0, 0, 0.08)",
+        "2xl": "0 10px 15px rgba(0, 0, 0, 0.1), 0 4px 6px rgba(0, 0, 0, 0.05)",
       },
     },
   },
