d9f4ae93b5416a88cd0a2a016deb2686ec759009
Motaz HM <motazhajm@gmail.com>
2024-11-26 22:43:11 +0200

    Adjust header styling

--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -9,7 +9,7 @@ const menu = [
   },
   {
     name: "Services",
-    href: "/Services",
+    href: "/services",
   },
   {
     name: "About Us",
@@ -27,11 +27,24 @@ const menu = [
 
 const Navbar = () => {
   return (
-    <header className="z-50 w-full h-20 p-4">
-      <Container className={"backdrop-blur-xl bg-background/60 h-[70px] rounded-xl flex items-center"}>
+    <header className="z-50 w-full h-20 p-4 ml-6">
+      <Container
+        className={
+          "backdrop-blur-xl bg-background/60 h-[70px] rounded-xl flex items-center"
+        }
+      >
         <div className="w-full flex items-center justify-between px-8">
-          <Link href="/" className="flex items-center gap-4 -ml-16 hover:scale-110 duration-300">
-            <Image src="/255-logo.svg" alt="logo" width={80} height={80} className="drop-shadow-lg" />
+          <Link
+            href="/"
+            className="flex items-center gap-1 -ml-14 xl:-ml-16 hover:scale-110 duration-300"
+          >
+            <Image
+              src="/255-logo.svg"
+              alt="logo"
+              width={80}
+              height={80}
+              className="drop-shadow-lg"
+            />
             <h1 className="text-xl font-bold drop-shadow-lg">255 Agency</h1>
           </Link>
           <nav>
@@ -39,7 +52,9 @@ const Navbar = () => {
               {menu.map((item) => (
                 <li key={item.name}>
                   <Link href={item.href}>
-                    <p className="hover:text-primary hover:scale-110 font-bold duration-300">{item.name}</p>
+                    <p className="hover:text-primary hover:scale-110 font-bold duration-300">
+                      {item.name}
+                    </p>
                   </Link>
                 </li>
               ))}
