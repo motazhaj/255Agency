33daf620c0d1a210c44bc114f0c9735b2450cb03
Motaz HM <motazhajm@gmail.com>
2024-12-02 15:53:04 +0200

    Adjust heaeder and container styling for consistent spacing

--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -28,41 +28,36 @@ const menu = [
 
 const Navbar = () => {
   return (
-    <header className="z-50 w-full h-20 p-4 ml-6">
-      <Container
-        className={
-          "backdrop-blur-xl bg-background/60 h-[70px] rounded-xl flex items-center"
-        }
-      >
-        <div className="w-full flex items-center justify-between px-8">
-          <Link
-            href="/"
-            className="flex items-center gap-1 -ml-14 xl:-ml-16 hover:scale-110 duration-300"
-          >
-            <Image
-              src="/255-logo.svg"
-              alt="logo"
-              width={80}
-              height={80}
-              className="drop-shadow-lg size-[80px]"
-            />
-            <h1 className="text-xl font-bold drop-shadow-lg">255 Agency</h1>
-          </Link>
-          <nav>
-            <ul className="gap-8 hidden md:flex">
-              {menu.map((item) => (
-                <li key={item.name}>
-                  <Link href={item.href}>
-                    <p className="hover:text-primary hover:scale-110 font-bold duration-300">
-                      {item.name}
-                    </p>
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </nav>
+    <header className="z-50 w-full h-20 py-4">
+      <Container>
+        <div className={"backdrop-blur-xl bg-background/60 h-[70px] rounded-xl flex items-center"}>
+          <div className="w-full flex items-center justify-between pr-6">
+            <Link href="/" className="flex items-center gap-1 hover:scale-110 duration-300">
+              <Image
+                src="/255-logo.svg"
+                alt="logo"
+                width={90}
+                height={90}
+                className="drop-shadow-lg size-[90px]"
+              />
+              <h1 className="text-xl font-bold drop-shadow-lg">255 Agency</h1>
+            </Link>
+            <nav>
+              <ul className="gap-8 hidden md:flex">
+                {menu.map((item) => (
+                  <li key={item.name}>
+                    <Link href={item.href}>
+                      <p className="hover:text-primary hover:scale-110 font-bold duration-300">
+                        {item.name}
+                      </p>
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </nav>
+          </div>
+          <MobileMenu />
         </div>
-        <MobileMenu />
       </Container>
     </header>
   );
