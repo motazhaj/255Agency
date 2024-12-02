689de1506a622f37919b32f5b199f6ca9caeb604
Motaz HM <motazhajm@gmail.com>
2024-12-02 15:53:19 +0200

    Update services page styling

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -1,3 +1,4 @@
+import BacktoHome from "@/components/layout/BacktoHome";
 import Container from "@/components/layout/Container";
 import ServiceCard from "@/components/services/ServiceCard";
 import React from "react";
@@ -20,22 +21,24 @@ const services = [
     description: "Develop a compelling brand identity that resonates with your audience.",
     image: "https://placehold.co/600x400",
     link: "/2",
-  },{
+  },
+  {
     title: "Graphic Design",
     description: "Develop a compelling brand identity that resonates with your audience.",
     image: "https://placehold.co/600x400",
     link: "/3",
-  }
+  },
 ];
 
 const page = () => {
   return (
     <Container>
-      <section className="bg-background/90 shadow-lg min-h-[800px] backdrop-blur-lg w-full rounded-xl flex flex-col items-center mt-24 p-8">
-        <h2 className="text-2xl font-bold text-primary px-4 mb-8">What we do at 255!</h2>
-        <p className="text-center text-foreground max-w-prose mb-12">
-          At 255, we help businesses grow by providing top-notch advertising solutions. Here's a glimpse of what we
-          offer:
+      <BacktoHome />
+      <section className="bg-background z-50 shadow-lg min-h-[800px] w-full rounded-xl flex flex-col mt-24 p-8">
+        <h2 className="text-2xl font-bold text-primary mb-2">What we do at 255!</h2>
+        <p className="text-foreground max-w-prose mb-12">
+          At 255, we help businesses grow by providing top-notch advertising solutions. Here's a
+          glimpse of what we offer:
         </p>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 w-full">
           {services.map((service) => (
