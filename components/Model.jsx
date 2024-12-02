7669ed732f46b3c08fbe51fc507b659924f2d49b
Motaz HM <motazhajm@gmail.com>
2024-12-02 15:45:18 +0200

    Impplement linking to main 3d model

--- components/Model.jsx
+++ components/Model.jsx
@@ -1,19 +1,21 @@
-import { useAnimations, useGLTF } from "@react-three/drei";
-import { useEffect, useRef } from "react";
+import { useGLTF } from "@react-three/drei";
+import { useRef } from "react";
+import LinkObject3D from "./Models3D/LinkObject3D";
 
 useGLTF.preload("/255.glb");
 
 export default function Model() {
   const group = useRef(null);
-  const { animations, scene } = useGLTF("/255.glb");
-  const { actions } = useAnimations(animations, scene);
+  const { scene } = useGLTF("/255.glb");
+  console.log(scene);
 
-  useEffect(() => {
-    console.log(actions);
-  }, []);
   return (
     <group ref={group}>
       <primitive object={scene} />
+      <LinkObject3D object={scene.children.find((c) => c.name === "services")} link="/services" />
+      <LinkObject3D object={scene.children.find((c) => c.name === "about")} link="/about" />
+      <LinkObject3D object={scene.children.find((c) => c.name === "contact")} link="/contact" />
+      <LinkObject3D object={scene.children.find((c) => c.name === "media")} link="/blog" />
     </group>
   );
 }
