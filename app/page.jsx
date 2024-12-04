98e17ca9b10826f5978b0362198f9f63e2393c1a
Motaz HM <motazhajm@gmail.com>
2024-12-04 22:52:17 +0200

    Add component to homepage

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,9 @@
+import ScrollToBottomButton from "@/components/layout/ScrollToBottom";
+
 export default function Home() {
   return (
-    <section className="z-50 bg-orange-500/20 w-full flex flex-col justify-center">
-      
+    <section className="-z-50 flex flex-col justify-center">
+      <ScrollToBottomButton />
     </section>
   );
 }
