a510e461531a2683a409ec9f7eeeaec4fc6c0d9b
Motaz HM <motazhajm@gmail.com>
2024-12-04 21:42:13 +0200

    Update page styling

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -1,84 +1,73 @@
 import BacktoHome from "@/components/layout/BacktoHome";
 import Container from "@/components/layout/Container";
+import PageHeader from "@/components/PageHeader";
 import ServiceCard from "@/components/services/ServiceCard";
 import React from "react";
 
 const services = [
   {
     title: "Graphic Design",
-    description:
-      "Develop a compelling brand identity that resonates with your audience.",
+    description: "Develop a compelling brand identity that resonates with your audience.",
     image: "https://placehold.co/600x400",
-    link: "/",
+    link: "/services/design",
   },
   {
     title: "Marketing",
-    description:
-      "Develop a compelling brand identity that resonates with your audience.",
+    description: "Develop a compelling brand identity that resonates with your audience.",
     image: "https://placehold.co/600x400",
-    link: "/1",
+    link: "/services/design",
   },
   {
     title: "Web Development",
-    description:
-      "Develop a compelling brand identity that resonates with your audience.",
+    description: "Develop a compelling brand identity that resonates with your audience.",
     image: "https://placehold.co/600x400",
-    link: "/2",
+    link: "/services/design",
   },
   {
     title: "Graphic Design",
-    description:
-      "Develop a compelling brand identity that resonates with your audience.",
+    description: "Develop a compelling brand identity that resonates with your audience.",
     image: "https://placehold.co/600x400",
-    link: "/3",
+    link: "/services/design",
   },
   {
     title: "Graphic Design",
-    description:
-      "Develop a compelling brand identity that resonates with your audience.",
+    description: "Develop a compelling brand identity that resonates with your audience.",
     image: "https://placehold.co/600x400",
-    link: "/3",
+    link: "/services/design",
   },
   {
     title: "Graphic Design",
-    description:
-      "Develop a compelling brand identity that resonates with your audience.",
+    description: "Develop a compelling brand identity that resonates with your audience.",
     image: "https://placehold.co/600x400",
-    link: "/3",
+    link: "/services/design",
   },
   {
     title: "Graphic Design",
-    description:
-      "Develop a compelling brand identity that resonates with your audience.",
+    description: "Develop a compelling brand identity that resonates with your audience.",
     image: "https://placehold.co/600x400",
-    link: "/3",
+    link: "/services/design",
   },
   {
     title: "Graphic Design",
-    description:
-      "Develop a compelling brand identity that resonates with your audience.",
+    description: "Develop a compelling brand identity that resonates with your audience.",
     image: "https://placehold.co/600x400",
-    link: "/3",
+    link: "/services/design",
   },
 ];
 
 const page = () => {
   return (
-    <div className="to-primary/80 from-orange-100 bg-gradient-to-b rounded-[40px] mt-32 mb-24">
+    <div className="bg-background rounded-[40px] mt-32 mb-24">
       <BacktoHome />
-      <section className="z-50 min-h-[800px] max-w-screen-xl mx-auto flex flex-col mt-24 py-12 gap-4 px-8">
-        <div className="w-full border-b-2 border-primary pb-2 mb-2">
-          <h2 className="text-2xl font-bold text-background mb-2">
-            What we do at 255!
-          </h2>
-          <p className="text-background font-bold">
-            At 255, we help businesses grow by providing top-notch advertising
-            solutions. Here's a glimpse of what we offer:
-          </p>
-        </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 w-full">
-          {services.map((service) => (
-            <ServiceCard {...service} />
+      <section className="z-50 min-h-[800px] max-w-screen-xl mx-auto flex flex-col mt-24 py-12 gap-4 px-8 ">
+        <PageHeader
+          text="Our Services!"
+          subtext="At 255, we help businesses grow by providing top-notch advertising solutions. Here's a glimpse of what we offer:"
+        />
+
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 w-full">
+          {services.map((service, index) => (
+            <ServiceCard {...service} key={index} />
           ))}
         </div>
       </section>
--- components/services/ServiceCard.jsx
+++ components/services/ServiceCard.jsx
@@ -3,12 +3,12 @@ import React from "react";
 
 const ServiceCard = ({ title, description, image, link = "#" }) => {
   return (
-    <Link href={link} className="bg-primary shadow-lg rounded-lg overflow-hidden hover:scale-105 duration-300">
+    <Link href={link} className="bg-primary shadow-xl rounded-lg overflow-hidden hover:scale-105 duration-300">
       <div className="rounded-full w-full">
-        <img src={image} alt={title} />
+        <img className="w-full" src={image} alt={title} />
       </div>
       <div className="px-6 pt-4 pb-8 flex flex-col">
-        <h3 className="text-lg font-bold text-background mb-2">{title}</h3>
+        <h3 className="text-xl font-bold text-background mb-2">{title}</h3>
         <p className="text-foreground font-semibold">{description}</p>
       </div>
     </Link>
