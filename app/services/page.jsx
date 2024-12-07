150673676b1e958148985e1ab3acf7157fbbb11e
Motaz HM <motazhajm@gmail.com>
2024-12-07 16:46:17 +0200

    Implement Page overlayed component in services page

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -1,5 +1,4 @@
-import BacktoHome from "@/components/layout/BacktoHome";
-import Container from "@/components/layout/Container";
+import PageOverlayed from "@/components/layout/PageOverlayed";
 import PageHeader from "@/components/PageHeader";
 import ServiceCard from "@/components/services/ServiceCard";
 import React from "react";
@@ -57,21 +56,18 @@ const services = [
 
 const page = () => {
   return (
-    <div className="bg-background rounded-[40px] mt-32 mb-24">
-      <BacktoHome />
-      <section className="z-50 min-h-[800px] max-w-screen-xl mx-auto flex flex-col mt-24 py-12 gap-4 px-8 ">
-        <PageHeader
-          text="Our Services!"
-          subtext="At 255, we help businesses grow by providing top-notch advertising solutions. Here's a glimpse of what we offer:"
-        />
+    <PageOverlayed>
+      <PageHeader
+        text="Our Services!"
+        subtext="At 255, we help businesses grow by providing top-notch advertising solutions. Here's a glimpse of what we offer:"
+      />
 
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 w-full">
-          {services.map((service, index) => (
-            <ServiceCard {...service} key={index} />
-          ))}
-        </div>
-      </section>
-    </div>
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 w-full">
+        {services.map((service, index) => (
+          <ServiceCard {...service} key={index} />
+        ))}
+      </div>
+    </PageOverlayed>
   );
 };
 
