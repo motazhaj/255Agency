e2ce7972512fdea5dc641972ffc944561a5c249f
Motaz HM <motazhajm@gmail.com>
2024-12-08 16:17:28 +0200

    Add services data js folder

---
+++ data/servicesData.js
@@ -0,0 +1,57 @@
+export const servicesData = [
+  {
+    id: 1,
+    title: "Design",
+    description:
+      "We provide creative designs that enhance your visual identity in all aspects, clearly conveying your message.",
+    image: "https://placehold.co/600x400",
+    slug: "design",
+  },
+  {
+    id: 2,
+    title: "Digital Marketing Service",
+    description:
+      "We build digital strategies to enhance your online presence across platforms, boosting customer engagement and sales.",
+    image: "https://placehold.co/600x400",
+    slug: "digital-marketing",
+  },
+  {
+    id: 3,
+    title: "Marketing Campaigns",
+    description:
+      "Integrated marketing strategies for attractive and distinctive online and offline campaigns that increase your brand awareness.",
+    image: "https://placehold.co/600x400",
+    slug: "campaigns",
+  },
+  {
+    id: 4,
+    title: "Interior and Exterior Design",
+    description:
+      "Attractive and modern interior and exterior designs for homes, companies, restaurants, and projects, reflecting beauty and functionality.",
+    image: "https://placehold.co/600x400",
+  },
+  {
+    id: 5,
+    title: "Photography, Video Shooting, and Editing",
+    description:
+      "Our professional photography, video shooting, and editing services highlight your brand's value.",
+    image: "https://placehold.co/600x400",
+    slug: "photography",
+  },
+  {
+    id: 6,
+    title: "Web and E-commerce, Software Solutions",
+    description:
+      "We offer integrated web and Online shop solutions along with professional software solutions to enhance your digital presence and optimize your project's performance efficiently.",
+    image: "https://placehold.co/600x400",
+    slug: "development",
+  },
+  {
+    id: 7,
+    title: "3D Animation",
+    description:
+      "We offer 3D animation solutions that enhance the viewer's experience and convey strong messages.",
+    image: "https://placehold.co/600x400",
+    slug: "3d-animation",
+  },
+];
