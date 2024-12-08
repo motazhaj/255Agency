0b5e1b6036c2de12c1bce48087dd38e65f6874a8
Motaz HM <motazhajm@gmail.com>
2024-12-08 12:21:23 +0200

    Add hero section to page overlay layout component

--- components/layout/PageOverlayed.jsx
+++ components/layout/PageOverlayed.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import BacktoHome from "./BacktoHome";
+import HeroSection from "./HeroSection";
 
-const PageOverlayed = ({ children }) => {
+const PageOverlayed = ({ title, image, children }) => {
   return (
-    <div className="bg-background rounded-[40px] mt-32 mb-4">
-      <BacktoHome />
-      <section className="z-50 min-h-[800px] max-w-screen-xl mx-auto flex flex-col mt-24 py-12 gap-4 px-8 ">
-        {children}
-      </section>
-    </div>
+    <>
+      <HeroSection image={image} title={title} />
+      <div className="bg-background rounded-[40px] mt-4 mb-4">
+        <BacktoHome />
+        <section className="z-50 min-h-[800px] max-w-screen-xl mx-auto flex flex-col py-12 gap-12 px-8 ">
+          {children}
+        </section>
+      </div>
+    </>
   );
 };
 
