3686385e37d3565d2d2e690bbde57eb340b4f18c
Motaz HM <motazhajm@gmail.com>
2024-12-25 20:37:56 +0200

    Adjust header styling and navigation

--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -29,20 +29,20 @@ export const navMenu = [
 const Navbar = () => {
   return (
     <header className="z-50 w-full h-20 py-4 fixed">
-      <div className={"h-[70px] rounded-full flex items-center"}>
-        <div className="w-full flex items-center justify-between px-6">
+      <div className={"h-[70px] flex items-center"}>
+        <div className="w-full flex items-center justify-between sm:px-6 px-2">
           <Link
             href="/"
-            className="flex items-center gap-1 hover:scale-110 duration-300 bg-primary/50 backdrop-blur rounded-full px-4 py-2"
+            className="flex items-center gap-1 hover:scale-110 duration-300 drop-shadow-[0_0_4px_black] px-4 py-2"
           >
             <Image
               src="/255-logo-white.svg"
               alt="logo"
               width={70}
               height={70}
-              className="size-[70px]"
+              className="sm:size-[70px] size-[60px]"
             />
-            <h1 className="text-2xl font-bold">255 Agency</h1>
+            <h1 className="text-xl sm:text-3xl font-bold">255 Agency</h1>
           </Link>
           <nav>
             <MobileMenu />
