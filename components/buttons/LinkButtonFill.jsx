b8204c6dae4a48e144c583a526ad5f38a8b26c6a
Motaz HM <motazhajm@gmail.com>
2025-05-16 19:00:48 +0300

    Adjust filled button styling

--- components/buttons/LinkButtonFill.jsx
+++ components/buttons/LinkButtonFill.jsx
@@ -6,7 +6,10 @@ const LinkButtonFill = ({ href, children, blank, className }) => {
     <Link
       href={href}
       {...(blank ? { target: "_blank" } : {})}
-      className={className + " px-8 py-4 flex gap-4 justify-center text-xl font-bold items-center rounded-xl text-slate-800 bg-gradient-to-t from-primary to-orange-500 transition drop-shadow-md hover:scale-105 hover:from-secondary hover:to-yellow-300"}
+      className={
+        className +
+        " px-6 py-3 flex gap-4 justify-center text-xl font-bold items-center rounded-xl text-foreground bg-background transition drop-shadow-md hover:scale-105 hover:bg-transparent/20"
+      }
     >
       {children}
     </Link>
