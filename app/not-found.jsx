b47f7931e2995aa775580235ee4d58660a4454c3
Motaz HM <motazhajm@gmail.com>
2025-05-22 14:19:47 +0300

    Update not found page with placeholder text

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,20 +1,13 @@
-import LinkButtonFill from "@/components/buttons/LinkButtonFill";
-import PageOverlayed from "@/components/layout/PageOverlayed";
-import PageHeader from "@/components/PageHeader";
+import Container from "@/components/layout/Container";
 import React from "react";
 
 const NotFound = () => {
   return (
-    <PageOverlayed title="404" image="https://placehold.co/1600x200">
-      <PageHeader
-        text="Page not found"
-        subtext="it seems that you have entered an invalid URL"
-        className="text-primary font-extrabold text-4xl sm:text-5xl lg:text-6xl"
-      />
-      <div className="mt-[100px] max-w-[200px] mx-auto">
-        <LinkButtonFill href="/">Back to Home</LinkButtonFill>
-      </div>
-    </PageOverlayed>
+    <section className="bg-foreground min-h-[calc(100vh-500px)] pt-80">
+      <Container className={"flex flex-col gap-8"}>
+        <h1 className="text-4xl font-bold text-primary">Coming Soon...</h1>
+      </Container>
+    </section>
   );
 };
 
