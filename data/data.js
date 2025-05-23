5a146ab9ea0b4d8ed98fbd9a79929e74d52b06ed
Motaz HM <motazhajm@gmail.com>
2025-05-23 14:17:24 +0300

    Add contact page

--- data/data.js
+++ data/data.js
@@ -20,7 +20,7 @@ export const navMenu2 = [
   },
   {
     name: "Contact Us",
-    href: "/",
+    href: "/contact",
   },
   {
     name: "Blog",
@@ -44,5 +44,26 @@ export const clientsData = [
       "At 255, the Creativity Factory, we turn power into art with HULK!🔥 \nWe're proud of our partnership with Hulk Burger Palestine, leading their digital marketing, advertising campaigns, visual branding, as well as content creation, photography, and video production",
     descriptionAr:
       "HULK! في 255 مصنع الإبداع، نحول القوة إلى فن مع 🔥 \nنفتخر بشراكتنا مع سلسلة مطاعم Hulk Burger حيث نقود إدارة التسويق الرقمي، والحملات الإعلانية، والهوية البصرية، بالإضافة إلى بناء المحتوى، والتصوير، والمونتاج",
+  },
+];
+
+export const contactInfo = [
+  {
+    name: "255 Nablus",
+    address:
+      "Al basateen st. - saad al deen building - 2nd floor 970 Nablus, Palestine",
+    phone: "+972 56-625-5255",
+    email: "255@255.ps",
+    whatsappLink: "https://wa.me/972566255255",
+    mapLink:
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2386.6822253193277!2d35.25553044513798!3d32.223670317700254!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x151ce1b1af5cb07d%3A0x8393738a2be6be73!2s255%20Advertising%20Agancy!5e0!3m2!1sen!2s!4v1733661511451!5m2!1sen!2s",
+  },
+  {
+    name: "255 Nazareth",
+    address: "Tech Hub Tower, 5th Floor, Al-Masyoun, Ramallah",
+  },
+  {
+    name: "255 Dubai",
+    address: "Business Bay Tower, Suite 800, Dubai, UAE",
   },
 ];
