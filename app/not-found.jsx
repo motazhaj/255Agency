e745fc4199747178d9a6cb8e1ef8be0f9521c88d
Motaz HM <motazhajm@gmail.com>
2025-05-23 15:52:47 +0300

    Update not found with a back home button

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,3 +1,4 @@
+import LinkButtonFill from "@/components/buttons/LinkButtonFill";
 import Container from "@/components/layout/Container";
 import React from "react";
 
@@ -5,7 +6,8 @@ const NotFound = () => {
   return (
     <section className="bg-foreground min-h-[calc(100vh-500px)] pt-80">
       <Container className={"flex flex-col gap-8"}>
-        <h1 className="text-4xl font-bold text-primary">Coming Soon...</h1>
+        <h1 className="text-4xl font-bold text-primary">404 Page Not Found</h1>
+        <LinkButtonFill href="/" className={"w-fit mr-auto bg-primary"}>Go Home</LinkButtonFill>
       </Container>
     </section>
   );
