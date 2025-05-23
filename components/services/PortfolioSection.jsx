5676c7b6c73de8394ca8981c64f8f63eeeb59ba0
Motaz HM <motazhajm@gmail.com>
2025-05-23 12:54:03 +0300

    Update data for potfolio section on home page

--- components/services/PortfolioSection.jsx
+++ components/services/PortfolioSection.jsx
@@ -3,54 +3,65 @@ import Container from "../layout/Container";
 import PortfolioItemCard from "./PortfolioItemCard";
 import LinkButtonFill from "../buttons/LinkButtonFill";
 import { FaArrowCircleRight } from "react-icons/fa";
+import FadeIn from "../animations/FadeIn";
+import FadeInBottom from "../animations/FadeInBottom";
+import FadeInRight from "../animations/FadeInRight";
+import FadeInLeft from "../animations/FadeInLeft";
 
 const portfolioItems = [
   {
-    img: "/designs/portfolio1.png",
+    img: "/portfolio/01.png",
     title: "Grand Nablus Bus CO.",
     link: "grand-nablus-bus-co",
   },
   {
-    img: "/designs/portfolio1.png",
-    title: "Grand Nablus Bus CO.",
-    link: "grand-nablus-bus-co",
+    img: "/portfolio/02.png",
+    title: "Chicken Crush",
+    link: "chicken-crush",
   },
   {
-    img: "/designs/portfolio1.png",
-    title: "Grand Nablus Bus CO.",
-    link: "grand-nablus-bus-co",
+    img: "/portfolio/03.png",
+    title: "Hulk Burger",
+    link: "hulk-burger",
   },
   {
-    img: "/designs/portfolio1.png",
-    title: "Grand Nablus Bus CO.",
-    link: "grand-nablus-bus-co",
+    img: "/portfolio/04.png",
+    title: "Akko Group",
+    link: "akko-group",
   },
 ];
 
 const PortfolioSection = () => {
   return (
     <section className="flex flex-col gap-8">
-      <h2 className="text-[70px] max-h-[110px] overflow-hidden flex items-center bg-background/30 -mt-12 animate-pulse min-w-[3000px] tracking-[.25em]">
-        O-PORTFOLIO-PORTFOLIO-PORTFOLIO-PORT FOLIO
-      </h2>
+      <FadeIn>
+        <h2 className="text-[70px] max-h-[110px] overflow-hidden flex items-center bg-background/30 -mt-12 animate-pulse min-w-[3000px] tracking-[.25em]">
+          O-PORTFOLIO-PORTFOLIO-PORTFOLIO-PORTFOLIO
+        </h2>
+      </FadeIn>
       <Container className={"mb-32"}>
-        <ul className="flex gap-8 justify-center">
-          {portfolioItems.map((item, index) => (
-            <li key={index}>
-              <PortfolioItemCard
-                img={item.img}
-                title={item.title}
-                link={item.link}
-              />
-            </li>
-          ))}
-        </ul>
-
-        <p className="text-center text-xl my-10">See our full Portfolio!</p>
-        <LinkButtonFill href="/portfolio" className={"w-fit mx-auto"}>
-          View Portfolio
-          <FaArrowCircleRight size={24} className="ml-2" />
-        </LinkButtonFill>
+        <FadeInBottom>
+          <ul className="flex gap-8 justify-center">
+            {portfolioItems.map((item, index) => (
+              <li key={index}>
+                <PortfolioItemCard
+                  img={item.img}
+                  title={item.title}
+                  link={item.link}
+                />
+              </li>
+            ))}
+          </ul>
+        </FadeInBottom>
+        <FadeInRight>
+          <p className="text-center text-xl my-10">See our full Portfolio!</p>
+        </FadeInRight>
+        <FadeInLeft>
+          <LinkButtonFill href="/portfolio" className={"w-fit mx-auto"}>
+            View Portfolio
+            <FaArrowCircleRight size={24} className="ml-2" />
+          </LinkButtonFill>
+        </  FadeInLeft>
       </Container>
     </section>
   );
